d5ddcd7ec3fcd33a3f865098386a47e6ba644722
jeremie-abt <jabt@student.42.fr>
2018-11-06 17:39:06 +0100

    champion ajoute

--- vm/champs/corru_wall.s
+++ vm/champs/corru_wall.s
@@ -1,10 +1,8 @@
 .name    "corrup_wall"
 .comment "Wai"
 
-
-zjmp %-5
-ld %178520071,	r2			# 0A A4 00 07
-ld %117440513,	r3			# 07 00 00 01
+ld %42991616,	r2			# 02 90 00 00
+ld %1,			r3			# 00 00 00 01
 ld %167770880,	r4			# 09 FF FB 00
 ld %0, r16
 sti r1, %:debut, %1
@@ -24,70 +22,50 @@ sti r1, %:sec_back_wall, %1
 back_wall:
 live %42
 
-st r2, -500
-st r3, -502
-st r1, -503
-st r4, -504
-st r1, -370
+st r2, -403
+st r3, -405
+st r1, -406
+st r4, -407
 
-st r2, -270
-st r3, -272
-st r4, -277
-st r1, -278
+st r2, -396
+st r3, -398
+st r1, -399
+st r4, -400
 
-st r2, -301
-st r3, -303
-st r4, -308
-st r1, -309
+st r2, -376
+st r3, -378
+st r1, -379
+st r4, -380
 
-st r2, -332
-st r3, -334
-st r4, -339
-st r1, -340
-
-st r2, -363
-st r3, -365
-st r4, -370
-st r1, -371
-
-st r2, -394
-st r3, -396
-st r4, -401
-st r1, -402
+st r2, -369
+st r3, -371
+st r1, -372
+st r4, -373
 
 zjmp %:back_wall
 
 sec_back_wall:
 live %42
-st r2, -239
-st r3, -241
-st r4, -246
-st r1, -247
 
-st r2, -270
-st r3, -272
-st r4, -277
-st r1, -278
+st r2, -353
+st r3, -355
+st r1, -356
+st r4, -357
 
-st r2, -301
-st r3, -303
-st r4, -308
-st r1, -309
+st r2, -346
+st r3, -348
+st r1, -349
+st r4, -350
 
-st r2, -332
-st r3, -334
-st r4, -339
-st r1, -340
+st r2, -326
+st r3, -328
+st r1, -329
+st r4, -330
 
-st r2, -363
-st r3, -365
-st r4, -370
-st r1, -371
-
-st r2, -394
-st r3, -396
-st r4, -401
-st r1, -402
+st r2, -319
+st r3, -321
+st r1, -322
+st r4, -323
 
 zjmp %:sec_back_wall
 
@@ -99,35 +77,25 @@ sti r1, %:sec_forward_wall, %1
 forward_wall:
 live %42
 
-st r2, 413
-st r3, 411
-st r4, 406
+st r2, 378
+st r3, 376
+st r1, 375
+st r4, 374
+
+st r2, 385
+st r3, 383
+st r1, 382
+st r4, 381
+
+st r2, 408
+st r3, 406
 st r1, 405
+st r4, 404
 
-st r2, 404
-st r3, 402
-st r4, 397
-st r1, 396
-
-st r2, 395
-st r3, 393
-st r4, 388
-st r1, 387
-
-st r2, 386
-st r3, 384
-st r4, 379
-st r1, 378
-
-st r2, 377
-st r3, 375
-st r4, 370
-st r1, 369
-
-st r2, 368
-st r3, 366
-st r4, 361
-st r1, 360
+st r2, 415
+st r3, 413
+st r1, 412
+st r4, 411
 
 zjmp %:forward_wall
 
@@ -135,35 +103,25 @@ sec_forward_wall:
 
 live%42
 
+st r2, 150
+st r3, 148
+st r1, 147
+st r4, 146
+
 st r2, 157
 st r3, 155
-st r4, 150
-st r1, 149
+st r1, 154
+st r4, 153
 
-st r2, 148
-st r3, 146
-st r4, 141
-st r1, 140
+st r2, 180
+st r3, 178
+st r1, 177
+st r4, 176
 
-st r2, 139
-st r3, 137
-st r4, 133
-st r1, 132
-
-st r2, 131
-st r3, 129
-st r4, 124
-st r1, 123
-
-st r2, 122
-st r3, 120
-st r4, 115
-st r1, 114
-
-st r2, 113
-st r3, 111
-st r4, 106
-st r1, 105
+st r2, 187
+st r3, 185
+st r1, 184
+st r4, 183
 
 zjmp %:sec_forward_wall
 
