138473d3aa129a034bcc2cba661e3203a721654d
Galemair42 <galemair@student.42.fr>
2018-11-06 13:35:21 +0100

    ok

--- vm/src/cw_init.c
+++ vm/src/cw_init.c
@@ -6,7 +6,7 @@
 /*   By: jabt <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 14:00:14 by jabt              #+#    #+#             */
-/*   Updated: 2018/11/05 18:00:32 by galemair         ###   ########.fr       */
+/*   Updated: 2018/11/06 13:33:02 by galemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void		cw_init_processeur(void)
 	ft_bzero(arena.memory, MEM_SIZE);
 	arena.champion = NULL;
 	ft_bzero(arena.process, sizeof(t_list *) * CYCLE_TO_DIE);
-	arena.awaiting_process = NULL;
 	arena.nb_champ = 0;
 	arena.cycle_to_die = CYCLE_TO_DIE;	
 	arena.max_check = MAX_CHECKS;
--- vm/src/instruction/cw_get_instructions_params.c
+++ vm/src/instruction/cw_get_instructions_params.c
@@ -6,12 +6,22 @@
 /*   By: galemair <galemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 14:17:12 by galemair          #+#    #+#             */
-/*   Updated: 2018/11/06 11:26:45 by jabt             ###   ########.fr       */
+/*   Updated: 2018/11/06 13:32:00 by galemair         ###   ########.fr       */
 /*   Updated: 2018/11/06 11:36:57 by galemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+/*
+**		Receive the OCP and the current PC
+**		First, it will isolate the 2 lightest bits to determine the size of the
+**		parameter
+**		Then it will read on the memory to calculate the value of the 
+**		parameters and will advance the current_PC
+**		Return -1 when an error occurs
+**		!! STILL HAVE TO MANAGE DIRECT PARAMETERS !! CARE IF IT STANDS FOR AN ADDRESS OR AN INTEGER	**
+*/
 
 static int		get_params1(unsigned int ocp, unsigned int *current_pc)
 {
@@ -32,16 +42,6 @@ static int		get_params1(unsigned int ocp, unsigned int *current_pc)
 	*current_pc += get_size(ocp);
 	return (value);
 }
-
-/*
-**		Receive the OCP and the current PC
-**		First, it will isolate the 2 lightest bits to determine the size of the
-**		parameter
-**		Then it will read on the memory to calculate the value of the 
-**		parameters and will advance the current_PC
-**		Return -1 when an error occurs
-**		!! STILL HAVE TO MANAGE DIRECT PARAMETERS !! CARE IF IT STANDS FOR AN ADDRESS OR AN INTEGER	**
-*/
 
 unsigned int		get_size(unsigned int ocp)
 {
