23d6cc48ada9769979c124301e34ee6092ae4538
jeremie-abt <jabt@student.42.fr>
2018-11-06 11:54:59 +0100

    suite test merge

--- libft/ft_lstappend.c
+++ libft/ft_lstappend.c
@@ -6,7 +6,7 @@
 /*   By: galemair <galemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 16:27:31 by galemair          #+#    #+#             */
-/*   Updated: 2018/11/05 16:34:00 by galemair         ###   ########.fr       */
+/*   Updated: 2018/11/06 10:40:27 by jabt             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstappend(t_list *lst, t_list *to_add)
 {
-	while (*lst)
-		*lst = (*lst)->next;
-	*lst->next = *to_add;
+	while (lst)
+		lst = lst->next;
+	lst->next = to_add;
 }
