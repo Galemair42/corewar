f654c65d387509804001aad481bd35b3c7c42242
jeremie-abt <jabt@student.42.fr>
2018-11-07 16:11:01 +0100

    work in progress

--- libft/ft_lstappend.c
+++ libft/ft_lstappend.c
@@ -6,11 +6,8 @@
 /*   By: galemair <galemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 16:27:31 by galemair          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/11/06 10:40:27 by jabt             ###   ########.fr       */
-=======
+/*   Updated: 2018/11/07 10:08:24 by jabt             ###   ########.fr       */
 /*   Updated: 2018/11/06 11:26:07 by galemair         ###   ########.fr       */
->>>>>>> a4442cc348eb72c604901f836f93aaf7021374f0
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +16,6 @@
 void	ft_lstappend(t_list *lst, t_list *to_add)
 {
 	while (lst)
-<<<<<<< HEAD
 		lst = lst->next;
-=======
-		lst = (lst)->next;
->>>>>>> a4442cc348eb72c604901f836f93aaf7021374f0
 	lst->next = to_add;
 }
