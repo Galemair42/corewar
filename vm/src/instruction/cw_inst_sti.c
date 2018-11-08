45d5665d44e1064355e5a0620d61904bd244138a
jeremie-abt <jabt@student.42.fr>
2018-11-08 15:14:40 +0100

    sti done

--- vm/src/instruction/cw_inst_sti.c
+++ vm/src/instruction/cw_inst_sti.c
@@ -18,9 +18,16 @@ void    cw_inst_sti(t_processus *process)
         return ;
     }
     if (((process->ocp >> 4) & 3) == IND_CODE)
-    {
         landing += process->params[1];
-    }
-    
+    else if (((process->ocp >> 4) & 3) == REG_CODE)
+        landing += process->reg[process->params[1]];
+    if ((((process->ocp >> 2) & 3) == IND_CODE))
+        landing += process->params[2]; 
+    else if (((process->ocp >> 4) & 3) == REG_CODE)
+        landing += process->reg[process->params[2]];
+    landing = MEM_MASK(landing);
+    apply_IDX_MOD(process->pc, MEM_MASK(landing));
+    cw_put_four_octet(landing, process->reg[process->params[0]]);
+    process->pc = ret; 
     cw_reset_process(process);
 }
