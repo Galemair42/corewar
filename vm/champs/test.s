6e36cbd39a70cb741497fd45480c29b3870d9fad
jeremie-abt <jabt@student.42.fr>
2018-11-09 18:26:40 +0100

    ok

--- vm/champs/test.s
+++ vm/champs/test.s
@@ -1,3 +1,7 @@
-.name    "Bonjour a tous"
-.comment "Wai"
-ldi r1,%2,r2
+
+#deassembler par msa 1.8, made by joe
+.name "Tching tching(Intercepteur), Bouh!Bouh!(bruits d'anti-jeu)"
+.comment ""
+
+st	r1, 5
+
