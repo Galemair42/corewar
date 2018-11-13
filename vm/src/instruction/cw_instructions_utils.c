cb54595102eedc54fc4064b6889c5bf2cda2f1d3
Galemair42 <galemair@student.42.fr>
2018-11-13 14:27:06 +0100

    IDX_MOD bug resolved

--- vm/src/instruction/cw_instructions_utils.c
+++ vm/src/instruction/cw_instructions_utils.c
@@ -6,7 +6,7 @@
 /*   By: galemair <galemair@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 13:39:56 by galemair          #+#    #+#             */
-/*   Updated: 2018/11/08 12:26:57 by galemair         ###   ########.fr       */
+/*   Updated: 2018/11/13 14:26:01 by galemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,11 @@ unsigned int	apply_IDX_MOD(unsigned int former_pc, unsigned int new_pc)
 		else
 		{
 			diff2 %= IDX_MOD;
-			diff2 -= former_pc;
-			if (diff2 < 0)
-				diff2 = 0;
-			return (0xFFF & (MEM_SIZE - diff2));
+			tmp = (int)(former_pc - diff2) >= 0 ? 0 : diff2 - former_pc; 
+			if (tmp == 0)
+				return (former_pc - diff2);
+			else
+				return (0xFFF & (MEM_SIZE - tmp));
 		}
 	}
 }
